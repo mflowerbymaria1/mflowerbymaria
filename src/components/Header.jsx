9049946cf10e5425f6b8997c377ce545b91ea260
Maria Flower <mariadeleandro.design@gmail.com>
2026-08-25 14:22:15 -0300

    Move logo image to the left side in main header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -143,7 +143,8 @@ export default function Header() {
 
       {/* Main Header */}
       <div className="main-header container">
-        <div className="header-left">
+        <div className="header-left" style={{ gap: '20px' }}>
+          <Logo size="small" link={true} />
           <span className="country-flag" title="Argentina">🇦🇷</span>
           <nav className="main-nav">
             <Link href="/">INICIO</Link>
@@ -191,10 +192,6 @@ export default function Header() {
               <Link href="/mayorista/login" className="nav-link font-bold" style={{ color: '#D47792', fontWeight: 800 }}>MAYORISTA</Link>
             )}
           </nav>
-        </div>
-
-        <div className="header-center">
-          <Logo size="small" link={true} />
         </div>
 
         <div className="header-right" style={{ position: 'relative' }} ref={cartRef}>
